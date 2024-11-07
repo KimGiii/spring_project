4d4b239429fe74958a2472fa350fdd27a891c75c
Gibo Kim <gibo9809@gmail.com>
2024-11-08 00:45:22 +0900

    first commit

---
+++ src/main/java/com/kingloo/web/KinglooApplication.java
@@ -0,0 +1,14 @@
+package com.kingloo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KinglooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinglooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingloo/web/KinglooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingloo.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinglooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
