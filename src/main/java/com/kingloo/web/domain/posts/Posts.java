ad14220a480c420db29a45feb921f8d0fec7feb9
Gibo Kim <gibo9809@gmail.com>
2024-11-08 00:47:30 +0900

    기본적인 등록/수정/조회 기능;;;;;;;;;;;

---
+++ src/main/java/com/kingloo/web/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.kingloo.web.domain.posts;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity // 테이블과 링크될 클래스임을 나타냄, 기본값으로 클래스의 카멜케이스 이름을 _으로 테이블 이름을 매칭
+public class Posts {
+
+    @Id // 해당 테이블의 PK 필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙을 나타냄
+    private long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
