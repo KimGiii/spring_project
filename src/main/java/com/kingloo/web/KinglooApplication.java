bcd8fd11ca0b64d2841b794d01370d6b31436395
kingool <gibo9809@gmail.com>
2024-11-29 01:02:24 +0900

    mustache를 활용한 시작, 게시글 작성 페이지 추가

--- src/main/java/com/kingloo/web/KinglooApplication.java
+++ src/main/java/com/kingloo/web/KinglooApplication.java
@@ -2,13 +2,13 @@ package com.kingloo.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class KinglooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KinglooApplication.class, args);
 	}
-
 }
